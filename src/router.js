a9693d660324f6c92d4dcf0de731ac7a898ff762
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-07-08 11:47:50 +0200

    implement user login

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const { json } = require('express/lib/response');
 const prisma = new PrismaClient();
+const secret = process.env.JWT_SECRET
 
 const router = express.Router();
 
@@ -37,7 +38,33 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    
+    const {username, password} = req.body
+
+    const foundUser = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+    })
+
+    if(!foundUser) {
+        return res.json({
+            message: 'No user found by this username'
+        })
+    }
+
+    const isMatch = await bcrypt.compare(password, foundUser.password)
+
+    if(!isMatch) {
+        return res.json({
+            message: 'Invalid password'
+        })
+    }
+
+    const token = jwt.sign(username, secret)
+
+    res.json({
+        token
+    })
 });
 
 module.exports = router;
